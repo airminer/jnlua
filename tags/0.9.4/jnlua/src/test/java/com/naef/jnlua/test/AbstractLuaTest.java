9c0861b2f11b0e930c5022329c4fa7d35ae2328d
andre@naef.com <andre@naef.com@a3548516-71f6-1919-9359-a55f8a62a8b3>
2012-01-29 14:44:25 +0000

    Tagging as 0.9.4

---
+++ tags/0.9.4/jnlua/src/test/java/com/naef/jnlua/test/AbstractLuaTest.java
@@ -0,0 +1,43 @@
+/*
+ * $Id$
+ * See LICENSE.txt for license terms.
+ */
+
+package com.naef.jnlua.test;
+
+import org.junit.After;
+import org.junit.Before;
+
+import com.naef.jnlua.LuaState;
+
+/**
+ * Abstract base class for JNLua unit tests.
+ */
+public abstract class AbstractLuaTest {
+	// ---- State
+	protected LuaState luaState;
+
+	// ---- Setup
+	/**
+	 * Performs setup.
+	 */
+	@Before
+	public void setup() throws Exception {
+		luaState = new LuaState();
+	}
+
+	/**
+	 * Performs teardown.
+	 */
+	@After
+	public void teardown() throws Throwable {
+		if (luaState != null) {
+			try {
+				luaState.close();
+			} catch (Throwable e) {
+				e.printStackTrace();
+				throw e;
+			}
+		}
+	}
+}
